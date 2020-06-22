bc689acb1ffb22bda0568e14b7a98ad7429791e8
lchj <627107345@qq.com>
2020-06-22 17:29:40 +0800

    文档

---
+++ src/文档.js
@@ -0,0 +1,20 @@
+// react项目新建
+//
+// 1. 新建一个目录 test-react
+// 2.cd  test-react  然后 yarn init
+// 3.yarn add webpack@3.10.0 --dev
+// 4. webpack 中文文档 https://www.webpackjs.com/concepts
+//     5.运行 npx webpack --config webpack.config.js 打包  npm run build
+// 6.安装插件HtmlWebpackPlugin   yarn add html-webpack-plugin@2.30.1 --dev
+// 7. 安装插件 babel-loader  yarn add babel-core@6.26.0 babel-preset-env@1.6.1 babel-loader@7.1.2 --dev
+// 8. 安装 yarn add babel-preset-react@6.24.1 --dev
+// 9.安装react    yarn add react@16.2.0 react-dom@16.2.0 --dev
+// 10.安装 yarn add style-loader@0.19.1 css-loader@0.28.8 --dev
+// 11. 安装 yarn add extract-text-webpack-plugin@3.0.2 --dev
+// 12. yarn add sass-loader@6.0.6 --dev
+// 13.yarn add node-sass@4.7.2 --dev 安装 node-sass 出现问题解决方法：  https://blog.csdn.net/zhu1500527791/article/details/53444870?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
+//     14.安图片资源： yarn add file-loader@1.1.6 url-loader@0.6.2 --dev
+// 15.yarn add font-awesome 字体图标库
+// 16.yarn add webpack-dev-server@2.9.7 --dev
+// 17.安装路由 yarn add react-router-dom@4.2.2
+// 18.https://webthemez.com/demo/insight-free-bootstrap-html5-admin-template/index.html
