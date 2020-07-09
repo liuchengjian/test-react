eb6ab256a4f0cdb3f8fb99435eb6a31941d96872
lchj <627107345@qq.com>
2020-07-09 10:18:10 +0800

    用户列表展示

--- src/page/user/index.jsx
+++ src/page/user/index.jsx
@@ -13,8 +13,8 @@ export default class UserList extends React.Component {
     super(props);
     this.state = {
       pageNum: 1,
-      total: 0,
       list: [],
+      firstLoading: true,
     }
   }
 
@@ -28,14 +28,50 @@ export default class UserList extends React.Component {
       this.setState({
         pageNum: res.pageNum,
         total: res.total,
-        list: res.list
+        list: res.list,
+      }, () => {
+        this.setState({
+          firstLoading: false,
+        })
       })
     }, errMsg => {
+      this.setState({
+        list: [],
+      });
       _mm.errorTips(errMsg)
     });
   }
 
+  /**
+   * 当页数发生变化时
+   * @param current
+   */
+  onPageChange(current) {
+    this.setState({
+      pageNum: current,
+    }, () => {
+      this.loadUserList()
+    })
+  }
+
   render() {
+    const {list, pageNum, total, firstLoading} = this.state;
+    let listBody = list.map((item, index) => {
+      return (
+        <tr key={index}>
+          <th className='text-center'>{item.id}</th>
+          <th className='text-center'>{item.username}</th>
+          <th className='text-center'>{item.email}</th>
+          <th className='text-center'>{item.phone}</th>
+          <th className='text-center'>{new Date(item.createTime).toLocaleString()}</th>
+        </tr>
+      )
+    });
+    let listError =
+      <tr>
+        <th colSpan={5} className='text-center'>{firstLoading ? "加载数据中~~" : "没有找到相应的数据~"}</th>
+      </tr>;
+    let tableBody = list.length > 0 ? listBody : listError;
     return (
       <div id="page-wrapper">
         <PageTitle title={"用户列表"}/>
@@ -44,31 +80,22 @@ export default class UserList extends React.Component {
             <table className="table table-striped table-bordered">
               <thead>
               <tr>
-                <th>{"用户名"}</th>
-                <th>{"邮箱"}</th>
-                <th>{"电话"}</th>
-                <th>{"是否是管理员"}</th>
-                <th>{"创建时间"}</th>
+                <th className='text-center'>{"ID"}</th>
+                <th className='text-center'>{"用户名"}</th>
+                <th className='text-center'>{"邮箱"}</th>
+                <th className='text-center'>{"电话"}</th>
+                <th className='text-center'>{"注册时间"}</th>
               </tr>
               </thead>
               <tbody>
-              {this.state.list.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    <th>{item.username}</th>
-                    <th>{item.email}</th>
-                    <th>{item.phone}</th>
-                    <th>{item.role === 1 ? "是" : "否"}</th>
-                    <th>{item.createTime}</th>
-                  </tr>
-                )
-              })}
-
+              {tableBody}
               </tbody>
             </table>
           </div>
         </div>
-        <Pagination current={this.state.pageNum} total={this.state.total}/>
+        <Pagination current={pageNum}
+                    total={total}
+                    onChange={(current, index) => this.onPageChange(current)}/>
       </div>
     );
   }
